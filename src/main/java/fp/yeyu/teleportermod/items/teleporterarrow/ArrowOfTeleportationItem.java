4bcd17264a4bb7574d2f4571d56224c7b6d7aaea
ye-yu <rafolwen98@gmail.com>
2020-04-22 02:10:14 +0800

    Remove logging

--- src/main/java/fp/yeyu/teleportermod/items/teleporterarrow/ArrowOfTeleportationItem.java
+++ src/main/java/fp/yeyu/teleportermod/items/teleporterarrow/ArrowOfTeleportationItem.java
@@ -6,18 +6,14 @@ import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class ArrowOfTeleportationItem extends ArrowItem {
-    public static final Logger LOGGER = LogManager.getLogger(ArrowOfTeleportationItem.class);
     public ArrowOfTeleportationItem(Item.Settings settings) {
         super(settings);
     }
 
     @Override
     public ProjectileEntity createArrow(World world, ItemStack stack, LivingEntity shooter) {
-        LOGGER.info("Created arrow.");
         return new ArrowOfTeleportationItemEntity(world, shooter);
     }
 }
