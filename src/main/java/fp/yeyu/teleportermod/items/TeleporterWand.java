4ff8c8aae7d6491e479f9c9ceeedd938e3281007
ye-yu <rafolwen98@gmail.com>
2020-04-27 18:33:29 +0800

    Use TeleporterWand differently on ascend and descend enchantment

--- src/main/java/fp/yeyu/teleportermod/items/TeleporterWand.java
+++ src/main/java/fp/yeyu/teleportermod/items/TeleporterWand.java
@@ -4,12 +4,15 @@ import fp.yeyu.teleportermod.TeleporterMod;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -23,6 +26,9 @@ import java.util.Objects;
 
 public class TeleporterWand extends Item {
     public static final String MOD_NAME = "teleporter_wand";
+    private enum Mode {
+        ASCEND, NORMAL, DESCEND
+    }
 
     public TeleporterWand(Settings settings) {
         super(settings);
@@ -43,6 +49,31 @@ public class TeleporterWand extends Item {
             return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
         }
 
+        Mode mode = getTeleportationMode(playerEntity.getStackInHand(hand));
+        if (mode == Mode.DESCEND) {
+            return descendEntity(world, playerEntity, hand);
+        }
+        return ascendEntity(world, playerEntity, hand, mode == Mode.ASCEND);
+    }
+
+    private Mode getTeleportationMode(ItemStack stackInHand) {
+        if (stackInHand.hasEnchantments()) {
+            ListTag enchantments = stackInHand.getEnchantments();
+            for(int i=0;i <enchantments.size();i++) {
+                final CompoundTag compound = enchantments.getCompound(i);
+                final String id = compound.getString("id");
+                if(id.equals("teleportermod:ascend")) {
+                    return Mode.ASCEND;
+                }
+                if(id.equals("teleportermod:descend")) {
+                    return Mode.DESCEND;
+                }
+            }
+        }
+        return Mode.NORMAL;
+    }
+
+    private TypedActionResult<ItemStack> ascendEntity(World world, PlayerEntity playerEntity, Hand hand, boolean hasAscendEnchantment) {
         boolean searchAir = false;
         int tickEffect = 20;
         Vec3d pos = playerEntity.getPos();
@@ -50,6 +81,10 @@ public class TeleporterWand extends Item {
             BlockState blockState = world.getBlockState(new BlockPos(pos));
             if (Objects.nonNull(blockState)) {
                 pos = pos.add(0, 1, 0);
+                if (blockState.getBlock() == Blocks.BEDROCK) { // if reached bedrock in nether, cancel
+                    playerEntity.playSound(SoundEvents.BLOCK_DISPENSER_FAIL, 1.0F, 1.0F);
+                    return new TypedActionResult<>(ActionResult.FAIL, playerEntity.getStackInHand(hand));
+                }
                 boolean isAirBlock = blockState.getMaterial() == Material.AIR;
                 if(isAirBlock) { // if not air block in the first place, dont do anything and keep going up
                     // we save teleportation point here
@@ -63,7 +98,11 @@ public class TeleporterWand extends Item {
             }
         }
 
-        if (!searchAir) { // solid block is not found
+        if (!searchAir) {// solid block is not found
+            if(hasAscendEnchantment) {
+                playerEntity.playSound(SoundEvents.BLOCK_DISPENSER_FAIL, 1.0F, 1.0F);
+                return new TypedActionResult<>(ActionResult.FAIL, playerEntity.getStackInHand(hand));
+            }
             pos = playerEntity.getPos(); // reset position
             pos = pos.add(0, 10, 0);
             tickEffect = 70; // increase duration
@@ -71,7 +110,8 @@ public class TeleporterWand extends Item {
 
         double x = pos.getX(), y = pos.getY(), z = pos.getZ();
         requestTeleport(world, playerEntity, x, y+0.5, z);
-        giveEntityEffect(playerEntity, tickEffect);
+        if(!hasAscendEnchantment)
+            giveEntityEffect(playerEntity, tickEffect);
         playerEntity.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1.0F, 1.0F);
 
         // add item damage
@@ -79,6 +119,29 @@ public class TeleporterWand extends Item {
         itemStack.damage(1, playerEntity, (p) -> p.sendToolBreakStatus(hand));
 
         return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    }
+
+    private TypedActionResult<ItemStack> descendEntity(World world, PlayerEntity playerEntity, Hand hand) {
+        BlockPos groundPos = playerEntity.getBlockPos().add(0, -1, 0);
+        Vec3d teleportingPos = null;
+        boolean searchGround = false;
+        for(int i=groundPos.getY(); i>0; i--) {
+            groundPos = groundPos.add(0, -1, 0);
+            BlockState groundBlock = world.getBlockState(groundPos);
+            if (groundBlock.getMaterial() == Material.AIR) {
+                searchGround = true;
+            } else if (searchGround) {
+                teleportingPos = new Vec3d(playerEntity.getX(), groundPos.getY() + 1, playerEntity.getZ());
+                break;
+            }
+        }
+        if (Objects.nonNull(teleportingPos)) {
+            requestTeleport(world, playerEntity, teleportingPos.getX(), teleportingPos.getY()+0.5, teleportingPos.getZ());
+            playerEntity.playSound(SoundEvents.ENTITY_ENDERMAN_TELEPORT, 1.0F, 1.0F);
+            return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+        }
+        playerEntity.playSound(SoundEvents.BLOCK_DISPENSER_FAIL, 1.0F, 1.0F);
+        return new TypedActionResult<>(ActionResult.FAIL, playerEntity.getStackInHand(hand));
     }
 
     private static void requestTeleport(World world, PlayerEntity playerEntity, double x, double y, double z) {
